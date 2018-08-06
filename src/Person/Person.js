b57f70a2ce0c0ba91df412b860624edab36ca82e
jltyler <john.lucas.tyler@gmail.com>
2018-08-06 13:28:07 -0600

    Ejected webpack config to change CSS modules
    Webpack CSS config uses CSS modules now
    Person.js imports and uses Person.css as a module
    Added global tags for name and title CSS classes

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 const printChilluns = (props) => {
     console.log("props.children:", props.children)
@@ -7,9 +7,9 @@ const printChilluns = (props) => {
 
 const person = (props) => {
     const x = (
-        <div className="namecard" >
-            <h2>Darth <span className="name">{props.name}</span></h2>
-            <h3 className="title">{props.title}</h3>
+        <div className={classes.namecard} >
+            <h2>Darth <span className={classes.name}>{props.name}</span></h2>
+            <h3 className={classes.title}>{props.title}</h3>
             {props.children && <p>{props.children}</p>}
             {props.textChange && <input type="text" onChange={props.textChange} value={props.name} />}
         </div>
