9c6c47bc6569a2f76f8b6d4e7f47086d10282bd5
Sumry <john.lucas.tyler@gmail.com>
2018-08-24 14:47:49 -0600

    Commented common.choice()

--- src/common.js
+++ src/common.js
@@ -1,5 +1,10 @@
 export default {
 
+// Choose random item
+// Multiple arguments: returns random choice from them
+// One array argument: returns random choice from that array
+// One non-array argument: returns that
+// No arguments: returns undefined
 choice(...args)
 {
     if (args.length === 0) return undefined;
